e7728c1aa52255194c507f6e943313eb78fcd826
mansoor <codehawk64@gmail.com>
2014-09-08 18:19:15 +0530

    latest

--- Assets/bikemove.cs
+++ Assets/bikemove.cs
@@ -6,6 +6,13 @@ public class bikemove : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 	
+		speed = 1;
+		moveThreshold = 0.2f;
+
+		movex = 0;
+
+		delay = 0.005f;
+
 	}
 
 
@@ -17,16 +24,45 @@ public class bikemove : MonoBehaviour {
 	bool goleft,goright;
 
 
+
+
+	float speed,moveThreshold,movex,iPx,delay,accel;
+
+
 	// Update is called once per frame
 	void Update () {
 
 
-		if (Input.GetKey (KeyCode.K)||Input.acceleration.x<-0.05f) {
+		iPx = Input.acceleration.x;
+
+
+
+
+/*
+	
+
+		if(Mathf.Abs (iPx)>moveThreshold)
+		{
+
+
+			movex=Mathf.Sign (iPx)*speed;
+
+			this.transform.Translate (Vector3.left * (movex));
+
+
+
+		}
+
+*/
+
+
+
+		if (Input.GetKey (KeyCode.K)||iPx>delay) {
 			goleft = true;
 			goright = false;
 			
 		} else
-		if (Input.GetKey (KeyCode.L)||Input.acceleration.x>0.05f) {
+		if (Input.GetKey (KeyCode.L)||iPx<-delay) {
 			goleft = false;
 			goright = true;
 			
@@ -38,26 +74,36 @@ public class bikemove : MonoBehaviour {
 
 
 
-
+		if (Mathf.Abs (iPx) < 1) {
+						accel = iPx/2;
+				}
 
 	//	if (Input.GetKey (KeyCode.K)) {
 		if(goleft)
 		{
 
-						this.transform.Translate (Vector3.left * (0.1f+degrees));
-			
+			if(Mathf.Abs(iPx)>delay)
+						this.transform.Translate (Vector3.left * (-(accel-delay)));
+		
+		//	this.transform.Translate (Vector3.left * (0.05f));
+
+
 						//	transform.RotateAround (target.position, Vector3.down, degrees * Time.deltaTime);
-						degrees += 0.005f;
+			//			degrees += 0.005f;
 			
 				} else
 			
 //		if (Input.GetKey (KeyCode.L)) {
 			if(goright)
 		{
+			if(Mathf.Abs(iPx)>delay)
+						this.transform.Translate (Vector3.left * (-(accel+delay)));
 
-						this.transform.Translate (-Vector3.left * (0.1f+degrees));
 
-						degrees += 0.005f;
+			//this.transform.Translate (Vector3.left * (-0.05f));
+
+
+			//			degrees += 0.005f;
 
 						//		transform.RotateAround (target.position, Vector3.forward, -degrees * Time.deltaTime);
 
@@ -65,6 +111,13 @@ public class bikemove : MonoBehaviour {
 			degrees=0;
 				}
 
+
+
+
+
+
+
+		print (""+transform.position.x);
 
 
 	
--- Assets/skeletonmove.cs
+++ Assets/skeletonmove.cs
@@ -28,28 +28,54 @@ public class skeletonmove : MonoBehaviour
 
 	bool goleft,goright;
 
+	float iPx;
+
+	int count;
+
 	void Update ()
 	{
+
+
+
+		if (count > 5) {
+						count = 0;
+				} else 
+		{
+			count++;		
+		}
+
+		if (count == 1) {
+			transform.Translate (-Vector3.up*0.03f);
+		} else if (count == 5) {
+			transform.Translate (Vector3.up*0.03f);
+
+				}
+
+
+		iPx = Input.acceleration.x;
+
+
 
 		Vector3 rot = spine.rotation.eulerAngles;
 		// Detect key input and add or subtract from the x rotation (scaling
 		// by deltaTime to make this speed independent from the frame rate)
 	
 
-		if (Input.GetKey (KeyCode.K)) {
-						goleft = true;
-						goright = false;
-
-				} else
-			if (Input.GetKey (KeyCode.L)) {
-						goleft = false;
-						goright = true;
-				
-				} else {
+		if (Input.GetKey (KeyCode.K)||iPx<-0.1f) {
+			goleft = true;
+			goright = false;
+			
+		} else
+		if (Input.GetKey (KeyCode.L)||iPx>0.1f) {
+			goleft = false;
+			goright = true;
+			
+		} else {
 			goleft=false;
 			goright=false;
-
-				}
+			
+		}
+		
 
 
 //		if (Input.GetKey (KeyCode.K)) {
@@ -59,7 +85,7 @@ public class skeletonmove : MonoBehaviour
 				
 						//		this.transform.Translate (Vector3.left * 0.1f);
 
-						curangles.z += 80 * Time.deltaTime;
+			curangles.z += 10*(2+Mathf.Abs(iPx/2)) * Time.deltaTime;
 
 				
 				} else
@@ -71,7 +97,7 @@ public class skeletonmove : MonoBehaviour
 			//		rot.z -= Time.deltaTime * 30.0f;
 						//		this.transform.Translate (-Vector3.left * 0.1f);
 
-						curangles.z -= 80 * Time.deltaTime;
+						curangles.z -= 10*(2+Mathf.Abs(iPx/2)) * Time.deltaTime;
 
 
 
@@ -79,9 +105,9 @@ public class skeletonmove : MonoBehaviour
 
 
 						if (curangles.z < 0)
-								curangles.z += 100 * Time.deltaTime;
+								curangles.z += 30 * Time.deltaTime;
 						if (curangles.z > 0)
-								curangles.z -= 100 * Time.deltaTime;
+								curangles.z -= 30 * Time.deltaTime;
 
 				}
 
@@ -104,17 +130,15 @@ public class skeletonmove : MonoBehaviour
 
 		*/
 
-		print ("This is log so ... "+rot.z);
-
 
 		// Apply the new rotation
 		//spine.rotation = Quaternion.Euler(rot);
 
-		curangles.x=Mathf.Clamp(curangles.x,-15,15);
+	//	curangles.x=Mathf.Clamp(curangles.x,-25,25);
 
-		curangles.y=Mathf.Clamp(curangles.y,-15,15);
+	//	curangles.y=Mathf.Clamp(curangles.y,-15,15);
 
-		curangles.z=Mathf.Clamp(curangles.z,-35,35);
+		curangles.z=Mathf.Clamp(curangles.z,-10,10);
 
 
 
